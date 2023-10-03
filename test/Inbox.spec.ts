09d925fe47f93edf2f2eb376f6eabb2afc195d4c
corkymaigre <corky.maigre@gmail.com>
2023-10-03 17:32:47 +0200

    Add unit tests for inbox contract

--- test/Inbox.spec.ts
+++ test/Inbox.spec.ts
@@ -1,24 +1,16 @@
-import assert from "assert";
 import ganache from "ganache";
 import * as fs from "fs";
-import { Contract, FMT_BYTES, FMT_NUMBER, Web3 } from "web3";
-import {
-  RegisteredSubscription,
-  SendTransactionEvents,
-} from "web3/lib/commonjs/eth.exports";
-import Web3PromiEvent from "web3-core-promievent";
+import { Contract, Web3 } from "web3";
+import { RegisteredSubscription } from "web3/lib/commonjs/eth.exports";
+import { Abi } from "solc";
 
 import compile from "../src/scripts/compile";
 
+const MESSAGE = "demo";
+
 const web3: Web3<RegisteredSubscription> = new Web3(ganache.provider());
 let accounts: string[];
-let inbox: Web3PromiEvent<
-  Contract<any>,
-  SendTransactionEvents<{
-    readonly number: FMT_NUMBER.BIGINT;
-    readonly bytes: FMT_BYTES.HEX;
-  }>
->;
+let inbox: Contract<Abi>;
 
 beforeAll(async () => {
   await compile();
@@ -31,7 +23,7 @@ beforeEach(async () => {
     fs.readFileSync("dist/contracts/Inbox.json") as unknown as string
   );
 
-  const args = ["Hello"];
+  const args = [MESSAGE];
   inbox = await new web3.eth.Contract(abi)
     .deploy({ data: bytecode, arguments: args as [] })
     .send({ from: accounts[0], gas: "1000000" });
@@ -39,6 +31,19 @@ beforeEach(async () => {
 
 describe("Inbox", () => {
   it("should deploy a contract", () => {
-    console.log(inbox);
+    expect(inbox.options.address).toBeDefined();
+  });
+
+  it("should has a default message", async () => {
+    const message: string = await inbox.methods.message().call();
+    expect(message).toBe(MESSAGE);
+  });
+
+  it("should be able to change message", async () => {
+    const msg = "foo";
+    // @ts-ignore
+    await inbox.methods.setMessage(msg).send({ from: accounts[0] });
+    const message: string = await inbox.methods.message().call();
+    expect(message).toBe(msg);
   });
 });
